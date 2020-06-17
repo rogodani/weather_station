7c8a2385128c21f4b17680198aacc6342a734bca
Daniel Rogozan <dan.rogozan@tora.com>
2020-06-17 22:02:03 +0300

    wind speed

---
+++ wind_speed/wind_speed_converter.py
@@ -0,0 +1,8 @@
+class Converter:
+    """Convert the wind speed from km/h in m/s"""
+
+    def __init__(self, speed):
+        self.speed = speed
+
+    def get_speed_in_ms(self):
+        return self.speed * 3.6
