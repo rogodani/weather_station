85b8fa679c14db1e8f97db2c339336984b1c00c9
Daniel Rogozan <dan.rogozan@tora.com>
2020-06-21 15:06:21 +0300

    testing speed class

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from weather_station.collecting_data.collecting_data import CollectingData
 from weather_station.wind_speed.speed_test import Speed
 from datetime import datetime
+from time import sleep
 
 CollectingData()._db_checkup()
-Speed().run()
+# Speed().run()
+
+while True:
+    Speed.half_spin_count = 0
+    sleep(5)
+    print("{0:.2f} km/h".format(Speed.calculate_wind_speed()))
+
 CollectingData.insert_data(datetime.now().isoformat(1, 1, 1, "N", 1))
